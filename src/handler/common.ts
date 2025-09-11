81ff9796c7db311e46191756ddfc4f4f8e3107d5
Abhinav kumar <62340895+sudo-abhinav@users.noreply.github.com>
2025-09-11 17:01:13 +0530

    Dev (#2)
    * test commit
    
    * add dev branch
    
    ---------
    
    Co-authored-by: abhinav131102 <abhinav.kumar@remotestate.com>

--- src/handler/common.ts
+++ src/handler/common.ts
@@ -18,9 +18,6 @@ export const testReq = async (req: Request, res: Response) => {
     method: req.method,
     path: req.path
   });
-
-  
-
   console.log(req)
   
   res.status(200).json({ message: "Request logged successfully" });
@@ -86,4 +83,3 @@ export const fetchAllrestaurants = async (req : Request , res :Response)=>{
     });
   }
   }
-
